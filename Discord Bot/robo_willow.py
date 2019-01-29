18f1cdb4eb84766980eac977eb5d147b4e266660
Alex Mauney <mathmauney@gmail.com>
2019-01-29 17:52:00 -0500

    Revert "Fix for PM"
    This reverts commit 051cd951b8a59193ca8d6bad753885d1db8eb6a6.

--- Discord Bot/robo_willow.py
+++ Discord Bot/robo_willow.py
@@ -344,8 +344,6 @@ async def on_message(message):
     Contains the help commands, and the bots ability to parse language.
 
     """
-    if message.server is not None:
-        taskmap = maps[message.server.id]
     if message.author == client.user:
         return
     elif message.content.startswith(bot_prefix):
@@ -438,6 +436,7 @@ async def on_message(message):
             await client.process_commands(message)
     elif prev_message_was_stop[message.server.id]:
         prev_message_was_stop[message.server.id] = False
+        taskmap = maps[message.server.id]
         try:
             task_name = message.content
             task = tasklist.find_task(task_name)
@@ -450,6 +449,7 @@ async def on_message(message):
         except pokemap.PokemapException as e:
             await client.send_message(message.channel, e.message)
     else:
+        taskmap = maps[message.server.id]
         try:
             stop_name = message.content
             prev_message_stop[message.server.id] = taskmap.find_stop(stop_name)
