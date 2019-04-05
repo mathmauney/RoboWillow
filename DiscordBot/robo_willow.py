0c485378a0b7c55bdcb2b835866670ce6a7fa62a
mathmauney <mathmauney@gmail.com>
2019-04-05 14:11:07 -0400

    Fix some help messages to make them use the right prefix

--- DiscordBot/robo_willow.py
+++ DiscordBot/robo_willow.py
@@ -148,7 +148,7 @@ async def addstop(ctx, *args):
             taskmap.save()
             await client.say('Creating stop named: ' + name + ' at [' + str(lat) + ', ' + str(long) + '].')
         except pokemap.PokemapException as e:
-            await client.say(e.message)
+            await cflient.say(e.message)
     else:
         await client.say('Not enough arguments. Please give the stop a name and the latitude and longitude. Use the "'+bot_prefix[0]+'help addstop" command for detailed instructions')
 
@@ -378,8 +378,8 @@ async def on_message(message):
                 msg = discord.Embed(colour=discord.Colour(0x186a0))
                 command_name = 'addstop'
                 command_help = """This command is used to add a new stop to the map, and can be used in two different ways.\n
-                The first is to specify the longitude and latitude like so '!addstop test 42.46 -76.51'
-                The second is to give an ingress intel url like so '!addstop test https://intel.ingress.com/intel?ll=42.447358,-76.48151&z=18&pll=42.46,-76.51'
+                The first is to specify the longitude and latitude like so '""" + bot_prefix[0] + """addstop test 42.46 -76.51'
+                The second is to give an ingress intel url like so '""" + bot_prefix[0] + """addstop test https://intel.ingress.com/intel?ll=42.447358,-76.48151&z=18&pll=42.46,-76.51'
 
                 A site like [this](geojson.io) can be used to find the latitude and longitude manually
                 While the [Ingress Intel Map](https://intel.ingress.com/intel) can be used to generate the url by clicking on a portal then clicking the Link button at the top right of the page."""
@@ -390,7 +390,7 @@ async def on_message(message):
                 command_name = 'addtask'
                 command_help = """This command lets you add a new task to the tasklist after research changes. If you do so please notify """ + maintainer_handle + """ so they can make sure new tasks show up correctly on the map.
 
-                The correct syntax for the command is !addtask reward quest shiny*
+                The correct syntax for the command is """ + bot_prefix[0] + """addtask reward quest shiny*
                 Values should be put in quotations if they are more than single words (shiny is optional).
                 shiny should be either 'True' or 'False'
                 """
@@ -407,7 +407,7 @@ async def on_message(message):
                 command_name = 'resetstop'
                 command_help = """This command removes any tasks associated with a stop. Use if a stop was misreported
 
-                The correct syntax for the command is !resetstop stop_name"""
+                The correct syntax for the command is """ + bot_prefix[0] + """resetstop stop_name"""
                 msg.add_field(name=command_name, value=command_help, inline=False)
                 await client.send_message(message.channel, embed=msg)
             elif 'settask' in message.content.lower():
@@ -415,7 +415,7 @@ async def on_message(message):
                 command_name = 'settask'
                 command_help = """This command assigns a task to a stop.
 
-                The correct syntax for the command is !settask reward stop_name
+                The correct syntax for the command is """ + bot_prefix[0] + """settask reward stop_name
                 If the reward is more than 1 word it should be enclosed with quotations marks
 
                 Tasks can also be assigned by saying the name of a stop then the name of a task (in different messages). If this is successful the bot should give a thumbs up to both messages."""
