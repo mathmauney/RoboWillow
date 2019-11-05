57f6955524c3d92df1501932c4ec8bb9ccb8c70f
mathmauney <mathmauney@gmail.com>
2019-11-05 10:10:43 -0500

    Add a main.

---
+++ robowillow/__main__.py
@@ -0,0 +1,25 @@
+import discord
+import sys
+from robowillow.core import Bot, logger
+
+
+def run_bot(debug=False):
+    """Sets up the bot, runs it and handles exit codes."""
+
+    bot = Bot(debug=debug)
+
+    bot.load_extension("robowillow.exts.mapping")
+    bot.load_extension("robowillow.core")
+
+    if bot.token is None or not bot.default_prefix:
+        bot.logger.critical(
+            "Token and prefix must be set in order to login.")
+        sys.exit(1)
+
+
+def main():
+    run_bot(debug=True)
+
+
+if __name__ == '__main__':
+    main()
