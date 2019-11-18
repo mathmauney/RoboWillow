f4911e3fa04ef505feb594257bd71a3a927dd603
mathmauney <mathmauney@gmail.com>
2019-11-18 15:52:23 -0500

    fix check

--- robowillow/core/checks.py
+++ robowillow/core/checks.py
@@ -1,7 +1,7 @@
 from discord.ext.commands import check
 
 
-async def check_is_owner(ctx):
+async def check_is_owner():
     async def _check(ctx):
         return await ctx.bot.is_owner(ctx.author)
-    return check(_check(ctx))
+    return check(_check)
