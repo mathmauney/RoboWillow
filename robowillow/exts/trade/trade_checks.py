929b32ef72134b309be0aa74e4b194da54fa0d9b
mathmauney <mathmauney@gmail.com>
2019-11-19 13:16:09 -0500

    Allow trades in DMs

--- robowillow/exts/trade/trade_checks.py
+++ robowillow/exts/trade/trade_checks.py
@@ -3,6 +3,8 @@ from robowillow.utils import database as db
 
 
 async def is_trade_channel(ctx):
+    if ctx.message.server is None:
+        return True
     channel_id = ctx.channel.id
     return db.check_permission(channel_id, 'trade')
 
