43fe6cd21266e0056887d8d7af2c657586ef7e3a
mathmauney <mathmauney@gmail.com>
2020-02-19 12:16:31 -0500

    Add command to turn on role commands.

--- robowillow/exts/admin/admin_cog.py
+++ robowillow/exts/admin/admin_cog.py
@@ -12,6 +12,7 @@ class Admin(Cog):
     @command()
     @has_permissions(administrator=True)
     async def tradehere(self, ctx, arg=True):
+        """Turn on/off trading commands in this channel."""
         if isinstance(arg, str):
             if arg.lower in ['t', 'yes', 'on', 'true']:
                 arg = True
@@ -27,6 +28,7 @@ class Admin(Cog):
     @command()
     @has_permissions(administrator=True)
     async def researchhere(self, ctx, arg=True):
+        """Turn on/off research commands and reporting in this channel."""
         if isinstance(arg, str):
             if arg.lower in ['t', 'yes', 'on', 'true']:
                 arg = True
@@ -36,5 +38,21 @@ class Admin(Cog):
             db.set_permission(ctx.channel.id, 'research', True)
         elif arg is False or arg == 0:
             db.set_permission(ctx.channel.id, 'research', False)
+        else:
+            ctx.send("Unable to understand. Use on or off as arguement for clarity.")
+
+    @command()
+    @has_permissions(administrator=True)
+    async def roleshere(self, ctx, arg=True):
+        """Turn on/off the ability to control roles in a channel."""
+        if isinstance(arg, str):
+            if arg.lower in ['t', 'yes', 'on', 'true']:
+                arg = True
+            elif arg.lower in ['n', 'no', 'off', 'false']:
+                arg = False
+        if arg is True or arg == 1:
+            db.set_permission(ctx.channel.id, 'notifier', True)
+        elif arg is False or arg == 0:
+            db.set_permission(ctx.channel.id, 'notifier', False)
         else:
             ctx.send("Unable to understand. Use on or off as arguement for clarity.")
