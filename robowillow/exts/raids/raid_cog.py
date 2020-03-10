748047b46c0fcad40c48557ca65d8c79c6244c45
mathmauney <mathmauney@gmail.com>
2020-03-10 12:52:48 -0400

    Add pokebattler infographic search.

--- robowillow/exts/raids/raid_cog.py
+++ robowillow/exts/raids/raid_cog.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Cog, command, has_permissions
 from robowillow.utils import pokemap
 from robowillow.utils import database as db
 from . import raid_checks
+import requests
 
 
 class Raids(Cog):
@@ -11,26 +12,27 @@ class Raids(Cog):
     def __init__(self, bot):
         """Start the raid cog."""
         self.bot = bot
+        response = requests.get("https://fight.pokebattler.com/raids")
+        self.raids = response.json()['tiers']
 
     @command()
     @raid_checks.raid_channel()
-    async def pokebattler(self, ctx, pokemon=None):
+    async def infographic(self, ctx, pokemon=None):
         """Return the pokebattler infographic for a pokemon if it exists."""
         if pokemon is None:
-            pokemon = db.raid_pokemon()
-        match = pokemap.match_pokemon(pokemon)
-        if match is None:
-            await ctx.send("Pokemon not found.")
-            return
-        url = "https://www.pokebattler.com/raids/defenders/" + match.upper() + "/levels/RAID_LEVEL_5/attackers/levels/35/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE/DEFENSE_RANDOM_MC?sort=ESTIMATOR&weatherCondition=NO_WEATHER&dodgeStrategy=DODGE_REACTION_TIME&aggregation=AVERAGE&randomAssistants=-1"
-        await ctx.send(url)   # TODO: try to grab the imgage and put it in
-        # TODO: have it fallback to the pokebattler page for the pokemon if there ins't an infographic
-
-    @command()
-    @has_permissions(administrator=True)
-    @raid_checks.raid_channel()
-    async def set_current(self, ctx, pokemon):
-        """Set the default 5* boss."""
-        match = pokemap.match_pokemon(pokemon)
-        db.raid_pokemon(match)
-        await ctx.message.add_reaction('👍')
+            pass
+        else:
+            match = pokemap.match_pokemon(pokemon)
+            if match is None:
+                await ctx.send("Pokemon not found.")
+                return
+            for tier in self.raids:
+                for index in self.raids[tier]['raids']:
+                    if match.upper() in self.raids[tier]['raids'][index]['pokemon']:
+                        if self.raids[tier]['raids'][index]['article'] is None:
+                            pass
+                        else:
+                            url = self.raids[tier]['raids'][index]['article']['infographicShareURL']
+                            await ctx.send(url)
+                            return
+            await ctx.send("Pokemon not found in pokebattler.")
