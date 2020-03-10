4ac93e63fa88d1a48d9ff104cb3b2464fa6b8d1f
mathmauney <mathmauney@gmail.com>
2020-03-10 11:24:54 -0400

    Add raidhere command

--- robowillow/exts/admin/admin_cog.py
+++ robowillow/exts/admin/admin_cog.py
@@ -58,3 +58,19 @@ class Admin(Cog):
             db.set_permission(ctx.channel.id, 'notifier', False)
         else:
             ctx.send("Unable to understand. Use on or off as arguement for clarity.")
+
+    @command()
+    @has_permissions(administrator=True)
+    async def raidhere(self, ctx, arg=True):
+        """Turn on/off the ability to control roles in a channel."""
+        if isinstance(arg, str):
+            if arg.lower in ['t', 'yes', 'on', 'true']:
+                arg = True
+            elif arg.lower in ['n', 'no', 'off', 'false']:
+                arg = False
+        if arg is True or arg == 1:
+            db.set_permission(ctx.channel.id, 'raids', True)
+        elif arg is False or arg == 0:
+            db.set_permission(ctx.channel.id, 'raids', False)
+        else:
+            ctx.send("Unable to understand. Use on or off as arguement for clarity.")
