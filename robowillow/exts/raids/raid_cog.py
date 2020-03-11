5d2255168229d1164f3502ee42ee0ff4bddaa485
mathmauney <mathmauney@gmail.com>
2020-03-11 13:25:48 -0400

    Add automated check for new pokebattler graphics.

--- robowillow/exts/raids/raid_cog.py
+++ robowillow/exts/raids/raid_cog.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Cog, command, has_permissions
 from robowillow.utils import pokemap
 from . import raid_checks
 import requests
+from datetime import datetime
 
 
 class Raids(Cog):
@@ -13,11 +14,22 @@ class Raids(Cog):
         self.bot = bot
         response = requests.get("https://fight.pokebattler.com/raids")
         self.raids = response.json()['tiers']
+        self.check_time = datetime.utcnow()
+        self.refresh_interval = 15  # How many minutes must elapse before requerying pokebattler
+
+    def check(self):
+        """Check for new raids if it has been long enough."""
+        delta = datetime.utcnow() - self.check_time()
+        if delta > 60*self.refresh_interval:
+            response = requests.get("https://fight.pokebattler.com/raids")
+            self.raids = response.json()['tiers']
+            self.check_time = datetime.utcnow()
 
     @command()
     @raid_checks.raid_channel()
     async def infographic(self, ctx, pokemon=None):
         """Return the pokebattler infographic for a pokemon if it exists."""
+        self.check()
         if pokemon is None:
             for tier in self.raids:
                 if tier['tier'] == "RAID_LEVEL_5":
