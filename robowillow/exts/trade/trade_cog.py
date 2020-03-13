2ee6c12c3e6380c9cdf50f982857ce3877953e71
mathmauney <mathmauney@gmail.com>
2020-03-13 09:26:08 -0400

    No await in get user?

--- robowillow/exts/trade/trade_cog.py
+++ robowillow/exts/trade/trade_cog.py
@@ -40,7 +40,7 @@ class Trader(Cog):
             match_user_id = match[2]
             if match_user_id not in offer_dict['notified']:
                 notification = "Trade matched! Your %s for <@%s>'s %s" % (want_str, str(sender.id), have_str)
-                match_user = await self.bot.get_user(match_user_id)
+                match_user = self.bot.get_user(match_user_id)
                 await match_user.send_message(notification)
                 tf.set_notified(offer, match_user_id)
             other_offer = tf.offers.find_one(match[3])
