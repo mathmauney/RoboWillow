495748375ab2c7e41397c39de57d833ed4ac6bde
Alex Mauney <mathmauney@gmail.com>
2020-08-06 11:46:41 -0700

    Better catch for matchuser = None

--- robowillow/exts/trade/trade_cog.py
+++ robowillow/exts/trade/trade_cog.py
@@ -41,10 +41,10 @@ class Trader(Cog):
             if match_user_id not in offer_dict['notified']:
                 notification = "Trade matched! Your %s for <@%s>'s %s" % (want_str, str(sender.id), have_str)
                 match_user = self.bot.get_user(match_user_id)
-                try:
+                if match_user is not None:
                     await match_user.send(notification)
-                except:
-                    pass
+                else:
+                    print(match_user_id)
                 tf.set_notified(offer, match_user_id)
             other_offer = tf.offers.find_one(match[3])
             tf.set_notified(other_offer, int(sender.id))
